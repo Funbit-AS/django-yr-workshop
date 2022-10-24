806f835f918f94a8261e701f12d795c9ea91fceb
Alex Simpson <alex.simpson@funbit.no>
2022-10-24 12:45:38 +0000

    adds request to yr to fill in bergen forecast with real data

--- weather/forecast/views.py
+++ weather/forecast/views.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from dateutil import parser
+
+import requests
 
 from django.utils import timezone
 from django.shortcuts import render
@@ -8,54 +11,57 @@ def forecast_bergen(request):
     """
     Collects weather forecast for Bergen and feeds it to the bergen template.
     """
+    # Fetch forceast for Bergen, ignoring any errors (to keep example code simple..!)
+    r = requests.get(
+        url="https://api.met.no/weatherapi/locationforecast/2.0/compact",
+        headers={"User-Agent": "DjangoWorkshop github.com/Funbit-AS/django-yr-workshop"},
+        params={"lat": 60.3929,"lon": 5.3241}
+    )
 
-    # ----------------------------
-    # YOUR MISSION
-    # ----------------------------
-    
-    # 1. Use the requests library to fetch data for Bergen from Yr
-    # 2. Edit the forecast dictionary below to use real data from the JSON response
+    # Requests can convert the Yr-response's JSON body into a nice Python dict for us
+    data = r.json()
 
-    # TIPS:
-    # - Yr API Docs: https://developer.yr.no/doc/GettingStarted/
-    # - Base url for API call: https://api.met.no/weatherapi/locationforecast/2.0/compact
-    # - Yr require that you set a User-Agent header, use "<Your Name> <Your Email>"
-    # - The coordinates from Bergen to use in your API request are:
-    # - - lat: 60.3929
-    # - - lon: 5.3241
-    # - Never use more coordinates with more than 4 decimal places or Yr may reject your call.
-    # - Do not alter the structure of the forecast dictionary, just fill it with real data.
-    # - The real data from Yr will have many more timeseries points than 3. 
+    # Now we need to wrangle the data into our forecast format
+    # - Again, no error handling for now, we blindly trust the Yr API docs.
 
+    # We can use Python's support for unpacking to assign lat, lon and altitude in one line!
+    latitude, longitude, altitude = data["geometry"]["coordinates"]
 
+    # Timeseries
+    # - Here we can use a little inline function to keep our code tidy
+    def parse_timeseries(datapoint):
+        """
+        Given a dict from the raw timeseries data, grab the interesting bits and
+        return as a new dict that is 'template ready'
+        """
+        measurements = datapoint["data"]["instant"]["details"]
+        
+        try:
+            # Watch out! Not all datapoints have symbols
+            symbol = datapoint["data"]["next_1_hours"]["summary"]["symbol_code"]
+        except KeyError:
+            symbol = ""
+
+        return {
+            "time": parser.parse(datapoint["time"]),
+            "air_temperature": measurements["air_temperature"],
+            "wind_speed": measurements["wind_speed"],
+            "cloud_area_fraction": measurements["cloud_area_fraction"],
+            "symbol": symbol
+        }
+
+    # Apply our helper function to each datapoint in the Yr timeseries list
+    timeseries = [
+        parse_timeseries(point) for point in data["properties"]["timeseries"]
+    ]
+
+    # Pass the real values we have gathered to the template
     forecast = {
-        "latitude": "0.0000",
-        "longitude": "0.0000",
-        "altitude": "0",
-        "symbol": "cloudy",  # Use symbol from first timeseries datapoint
-        "timeseries": [
-            {
-                "time": timezone.make_aware(datetime(2022,11,1,12,0)),
-                "air_temperature": 8.2,
-                "wind_speed": 0.8,
-                "cloud_area_fraction": 100.0,
-                "symbol": "cloudy"
-            },
-            {
-                "time": timezone.make_aware(datetime(2022,11,1,13,0)),
-                "air_temperature": 7.9,
-                "wind_speed": 2.2,
-                "cloud_area_fraction": 94.5,
-                "symbol": "rain"
-            },
-            {
-                "time": timezone.make_aware(datetime(2022,11,1,14,0)),
-                "air_temperature": 7.7,
-                "wind_speed": 3.2,
-                "cloud_area_fraction": 88.9,
-                "symbol": "rainandthunder"
-            }
-            # ... more
-        ]
+        "latitude":latitude,
+        "longitude": longitude,
+        "altitude": altitude,
+        "symbol": timeseries[0]["symbol"],  # Use symbol from first timeseries datapoint
+        "timeseries": timeseries
     }
+
     return render(request, "forecast/bergen.html", context={"forecast": forecast})
